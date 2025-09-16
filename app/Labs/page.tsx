5fceeb6ca1861618a85d1a66cddcfd275cfb3ed1
clairestewart03 <clairestewart03@icloud.com>
2025-09-16 14:41:13 -0400

    1.3.3 to 1.3.8 and code from lecture 2

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -5,13 +5,13 @@ export default function Labs() {
             <h1>Labs</h1>
             <ul>
                 <li>
-                    <Link href="/labs/lab1" id="wd-lab1-link">
+                    <Link href="/Labs/Lab1" id="wd-lab1-link">
                         Lab 1: HTML Examples </Link> </li>
                 <li>
-                    <Link href="/labs/lab2" id="wd-lab2-link">
+                    <Link href="/Labs/Lab2" id="wd-lab2-link">
                         Lab 2: CSS Basics </Link> </li>
                 <li>
-                    <Link href="/labs/lab3" id="wd-lab3-link">
+                    <Link href="/Labs/Lab3" id="wd-lab3-link">
                         Lab 3: JavaScript Fundamentals </Link> </li>
             </ul>
         </div>
